c6d29d16765936172fdc70252991916608186052
crazyoyo <crazyoyo@qq.com>
2025-08-18 12:08:24 +0800

    bug fix for assign golang version in Dockerfile.

--- infra-iac/init.sh
+++ infra-iac/init.sh
@@ -6,6 +6,9 @@ setup_environment() {
   # Get CloudFormation stack ID
   STACK_ID=$(grep "^StackName=" /tmp/e2b.log | cut -d'=' -f2)
   
+  # Validate stack existence
+  [[ -z "$STACK_ID" ]] && { echo "Error: Failed to get CloudFormation Stack ID"; exit 1; }
+
   # Dynamic export of CFN outputs
   declare -A CFN_OUTPUTS
   while IFS=$'\t' read -r key value; do
